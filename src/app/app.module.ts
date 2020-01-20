16f8c60ebb0b9546f7348c729cfa9b7154253d2c
Skander Haouam <44363467+madnaks@users.noreply.github.com>
2020-01-21 00:40:58 +0100

    New Header & Fonts Commit
    Add new component Header
    Add Google fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactMeComponent
+    ContactMeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
