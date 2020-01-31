727d2dbab944c286a66f911a7726793b11872210
Skander Haouam <44363467+madnaks@users.noreply.github.com>
2020-01-31 23:54:13 +0100

    Adjusment Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './core/header/header.component';
 import { CvComponent } from './cv/cv.component';
+import { FourOhFourComponent } from './core/four-oh-four/four-oh-four.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ExperienceComponent } from './cv/experience/experience.component';
+import { FormationComponent } from './cv/formation/formation.component';
+import { TraineeshipComponent } from './cv/traineeship/traineeship.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +20,17 @@ import { CvComponent } from './cv/cv.component';
     HomeComponent,
     ContactMeComponent,
     HeaderComponent,
-    CvComponent
+    CvComponent,
+    FourOhFourComponent,
+    FooterComponent,
+    ExperienceComponent,
+    FormationComponent,
+    TraineeshipComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
@@ -11,5 +12,7 @@ export class ContactMeComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
 }
