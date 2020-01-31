8ee903aa732bee4e6fc039b322d3ab14a3eb087b
Skander Haouam <44363467+madnaks@users.noreply.github.com>
2020-01-31 23:50:07 +0100

    404 Commit
    Add a 404 page for unkown pages or others

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { CvComponent } from './cv/cv.component';
+import { FourOhFourComponent } from './core/four-oh-four/four-oh-four.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path:'home', component: HomeComponent },
   { path:'contact-me', component : ContactMeComponent},
-  { path:'cv',component : CvComponent}
+  { path:'cv',component : CvComponent},
+  { path:'not-found', component: FourOhFourComponent},
+  { path:'**', redirectTo: 'not-found'}
 ];
 
 @NgModule({
